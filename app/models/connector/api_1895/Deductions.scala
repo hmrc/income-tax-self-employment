55f7671f0eb273b116a9defd5dd2b6aa1f62ae99
Harry Ritchie <111759395+harry-ritchie@users.noreply.github.com>
2023-11-06 09:37:18 +0000

    SASS-5855: Add API 1895 model

---
+++ app/models/connector/api_1895/Deductions.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.connector.api_1895
+
+import play.api.libs.json.{Format, Json}
+
+case class Deductions(costOfGoods: Option[SelfEmploymentDeductionsDetailPosNegType],
+                      constructionIndustryScheme: Option[SelfEmploymentDeductionsDetailType],
+                      staffCosts: Option[SelfEmploymentDeductionsDetailType],
+                      travelCosts: Option[SelfEmploymentDeductionsDetailType],
+                      premisesRunningCosts: Option[SelfEmploymentDeductionsDetailPosNegType],
+                      maintenanceCosts: Option[SelfEmploymentDeductionsDetailPosNegType],
+                      adminCosts: Option[SelfEmploymentDeductionsDetailType],
+                      businessEntertainmentCosts: Option[SelfEmploymentDeductionsDetailType],
+                      advertisingCosts: Option[SelfEmploymentDeductionsDetailType],
+                      interest: Option[SelfEmploymentDeductionsDetailPosNegType],
+                      financialCharges: Option[SelfEmploymentDeductionsDetailPosNegType],
+                      badDebt: Option[SelfEmploymentDeductionsDetailPosNegType],
+                      professionalFees: Option[SelfEmploymentDeductionsDetailAllowablePosNegType],
+                      depreciation: Option[SelfEmploymentDeductionsDetailPosNegType],
+                      other: Option[SelfEmploymentDeductionsDetailType],
+                      simplifiedExpenses: Option[BigDecimal])
+
+object Deductions {
+  implicit lazy val deductionsTypeJsonFormat: Format[Deductions] = Json.format[Deductions]
+}
