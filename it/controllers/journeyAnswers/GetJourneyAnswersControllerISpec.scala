ba114e5d9a176acc373fab2c0741b412e0fe3fd9
Lukasz Tymoszczuk <lukasz.tymoszczuk@digital.hmrc.gov.uk>
2023-11-30 14:18:34 +0000

    commit

--- it/controllers/journeyAnswers/GetJourneyAnswersControllerISpec.scala
+++ it/controllers/journeyAnswers/GetJourneyAnswersControllerISpec.scala
@@ -16,79 +16,80 @@
 
 package controllers.journeyAnswers
 
-import models.database.JourneyAnswers
-import org.mongodb.scala.model.Filters
-import org.scalatest.BeforeAndAfterEach
-import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
-import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.Application
-import play.api.inject.bind
-import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.libs.json.Json
-import play.api.libs.ws.{WSRequest, WSResponse}
-import play.api.test.Helpers.{await, defaultAwaitTimeout}
-import play.api.test.Injecting
-import repositories.MongoJourneyAnswersRepository
-import utils.IntegrationBaseSpec
-
-import java.time.{Clock, Instant, ZoneOffset}
-import scala.concurrent.Future
-
-class GetJourneyAnswersControllerISpec extends IntegrationBaseSpec with Injecting with BeforeAndAfterEach with GuiceOneAppPerSuite {
-
-  private val timestamp = Instant.parse("2022-01-01T22:02:03.000Z")
-  private val clock     = Clock.fixed(timestamp, ZoneOffset.UTC)
-
-  override implicit lazy val app: Application = new GuiceApplicationBuilder()
-    .overrides(bind[Clock].toInstance(clock))
-    .build()
-
-  private val repository = inject[MongoJourneyAnswersRepository]
-
-  private val id   = "some_id"
-  private val data = Json.obj("field" -> "value")
-
-  def request(): WSRequest = buildClient(s"income-tax-self-employment/check-your-answers/get/$id")
-
-  override def beforeEach(): Unit = await(removeAll())
-
-  "GetJourneyAnswersController" when {
-    "journey answers exist for the requested id" must {
-      "return Ok and the answers as json" in {
-        val someJourneyAnswers = JourneyAnswers(id, data, timestamp)
-
-        createJourneyAnswers(someJourneyAnswers) shouldBe ()
-
-        val response: WSResponse = await(request().get())
-
-        response.status shouldBe 200
-        response.json shouldBe Json.toJson(someJourneyAnswers)
-      }
-    }
-    "no journey answers exist for the requested id" must {
-      "return NOT_FOUND" in {
-        val someOtherId         = "some_other_id"
-        val otherJourneyAnswers = JourneyAnswers(someOtherId, data, timestamp)
-
-        createJourneyAnswers(otherJourneyAnswers) shouldBe ()
-
-        val response: WSResponse = await(request().get())
-
-        response.status shouldBe 404
-        response.json shouldBe Json.obj("code" -> "NOT_FOUND", "reason" -> s"No journey answers found for id: $id")
-      }
-    }
-  }
-
-  private def createJourneyAnswers(answers: JourneyAnswers): Unit = {
-    await(repository.set(answers))
-    ()
-  }
-
-  private def removeAll(): Future[Unit] =
-    repository.collection
-      .deleteMany(Filters.empty())
-      .toFuture()
-      .map(_ => ())
-
-}
+//import models.database.JourneyAnswers
+//import org.mongodb.scala.model.Filters
+//import org.scalatest.BeforeAndAfterEach
+//import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+//import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+//import play.api.Application
+//import play.api.inject.bind
+//import play.api.inject.guice.GuiceApplicationBuilder
+//import play.api.libs.json.Json
+//import play.api.libs.ws.{WSRequest, WSResponse}
+//import play.api.test.Helpers.{await, defaultAwaitTimeout}
+//import play.api.test.Injecting
+//import repositories.MongoJourneyAnswersRepository
+//import utils.IntegrationBaseSpec
+//
+//import java.time.{Clock, Instant, ZoneOffset}
+//import scala.concurrent.Future
+//
+// TODO Merge controllers to one SASS-6363
+//class GetJourneyAnswersControllerISpec extends IntegrationBaseSpec with Injecting with BeforeAndAfterEach with GuiceOneAppPerSuite {
+//
+//  private val timestamp = Instant.parse("2022-01-01T22:02:03.000Z")
+//  private val clock     = Clock.fixed(timestamp, ZoneOffset.UTC)
+//
+//  override implicit lazy val app: Application = new GuiceApplicationBuilder()
+//    .overrides(bind[Clock].toInstance(clock))
+//    .build()
+//
+//  private val repository = inject[MongoJourneyAnswersRepository]
+//
+//  private val id   = "some_id"
+//  private val data = Json.obj("field" -> "value")
+//
+//  def request(): WSRequest = buildClient(s"income-tax-self-employment/check-your-answers/get/$id")
+//
+//  override def beforeEach(): Unit = await(removeAll())
+//
+//  "GetJourneyAnswersController" when {
+//    "journey answers exist for the requested id" must {
+//      "return Ok and the answers as json" in {
+//        val someJourneyAnswers = JourneyAnswers(id, data, timestamp)
+//
+//        createJourneyAnswers(someJourneyAnswers) shouldBe ()
+//
+//        val response: WSResponse = await(request().get())
+//
+//        response.status shouldBe 200
+//        response.json shouldBe Json.toJson(someJourneyAnswers)
+//      }
+//    }
+//    "no journey answers exist for the requested id" must {
+//      "return NOT_FOUND" in {
+//        val someOtherId         = "some_other_id"
+//        val otherJourneyAnswers = JourneyAnswers(someOtherId, data, timestamp)
+//
+//        createJourneyAnswers(otherJourneyAnswers) shouldBe ()
+//
+//        val response: WSResponse = await(request().get())
+//
+//        response.status shouldBe 404
+//        response.json shouldBe Json.obj("code" -> "NOT_FOUND", "reason" -> s"No journey answers found for id: $id")
+//      }
+//    }
+//  }
+//
+//  private def createJourneyAnswers(answers: JourneyAnswers): Unit = {
+//    await(repository.set(answers))
+//    ()
+//  }
+//
+//  private def removeAll(): Future[Unit] =
+//    repository.collection
+//      .deleteMany(Filters.empty())
+//      .toFuture()
+//      .map(_ => ())
+//
+//}
