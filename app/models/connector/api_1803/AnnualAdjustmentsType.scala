ac7a0305453c504284944a3996d9dcc0aaea9ef1
Lukasz Tymoszczuk <lukasz.tymoszczuk@digital.hmrc.gov.uk>
2023-12-11 17:05:04 +0000

    SASS-6510: Model get income: API#1786 and API#1803

---
+++ app/models/connector/api_1803/AnnualAdjustmentsType.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.connector.api_1803
+
+import play.api.libs.json._
+
+/** Represents the Swagger definition for annualAdjustmentsType.
+  */
+case class AnnualAdjustmentsType(
+    includedNonTaxableProfits: Option[BigDecimal],
+    basisAdjustment: Option[BigDecimal],
+    overlapReliefUsed: Option[BigDecimal],
+    accountingAdjustment: Option[BigDecimal],
+    averagingAdjustment: Option[BigDecimal],
+    lossBroughtForward: Option[BigDecimal],
+    outstandingBusinessIncome: Option[BigDecimal],
+    balancingChargeBpra: Option[BigDecimal],
+    balancingChargeOther: Option[BigDecimal],
+    goodsAndServicesOwnUse: Option[BigDecimal]
+)
+
+object AnnualAdjustmentsType {
+  implicit lazy val annualAdjustmentsTypeJsonFormat: Format[AnnualAdjustmentsType] = Json.format[AnnualAdjustmentsType]
+}
---
+++ app/models/connector/api_1803/AnnualNonFinancialsType.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.connector.api_1803
+
+import play.api.libs.json._
+
+/** Represents the Swagger definition for annualNonFinancialsType.
+  * @param businessDetailsChangedRecently
+  *   The details of the business recently changed
+  * @param exemptFromPayingClass4Nics
+  *   The individual is exempt from paying NI contributions towards Class 4
+  * @param class4NicsExemptionReason
+  *   Exemption Reason - 001 - Non Resident, 002 - Trustee, 003 - Diver, 004 - Employed earner taxed under ITTOIA 2005, 005 - Over state pension age,
+  *   006 - Under 16
+  */
+case class AnnualNonFinancialsType(
+    businessDetailsChangedRecently: Option[Boolean],
+    exemptFromPayingClass4Nics: Option[Boolean],
+    class4NicsExemptionReason: Option[AnnualNonFinancialsType.Class4NicsExemptionReason.Value]
+)
+
+object AnnualNonFinancialsType {
+  implicit lazy val annualNonFinancialsTypeJsonFormat: Format[AnnualNonFinancialsType] = Json.format[AnnualNonFinancialsType]
+
+  // noinspection TypeAnnotation
+  object Class4NicsExemptionReason extends Enumeration {
+    val _001 = Value("001")
+    val _002 = Value("002")
+    val _003 = Value("003")
+    val _004 = Value("004")
+    val _005 = Value("005")
+    val _006 = Value("006")
+
+    type Class4NicsExemptionReason = Value
+    implicit lazy val Class4NicsExemptionReasonJsonFormat: Format[Value] = Format(Reads.enumNameReads(this), Writes.enumNameWrites[this.type])
+  }
+}
