81597da013905e0a90b8be26e95796d9b2848238
Lukasz Tymoszczuk <lukasz.tymoszczuk@digital.hmrc.gov.uk>
2023-12-27 15:51:24 +0000

    commit

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -34,7 +34,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 87,
+    ScoverageKeys.coverageMinimumStmtTotal := 80,
     ScoverageKeys.coverageFailOnMinimum    := true,
     ScoverageKeys.coverageHighlighting     := true
   )
