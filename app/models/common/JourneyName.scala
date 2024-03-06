12ea1f13201ef7d3d328ee2ef6d9d460454ad1fa
Jacob Raffe <jakeraffe24@gmail.com>
2024-03-06 10:38:47 +0000

    [SASS-7237] Add AnnualInvestmentAllowance to Journey list

--- app/models/common/JourneyName.scala
+++ app/models/common/JourneyName.scala
@@ -49,6 +49,7 @@ object JourneyName extends Enum[JourneyName] with utils.PlayJsonEnum[JourneyName
   case object ZeroEmissionGoodsVehicle    extends JourneyName("capital-allowances-zero-emission-goods-vehicle")
   case object ElectricVehicleChargePoints extends JourneyName("capital-allowances-electric-vehicle-charge-points")
   case object BalancingAllowance          extends JourneyName("capital-allowances-balancing-allowance")
+  case object AnnualInvestmentAllowance   extends JourneyName("capital-allowances-annual-investment-allowance")
 
   // Are we using this code? Should we be if not?
   implicit def pathBindable(implicit strBinder: PathBindable[String]): PathBindable[JourneyName] = new PathBindable[JourneyName] {
