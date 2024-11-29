eb17d59cacf25dd6bc3708afe9b35c8106eea06d
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2024-11-29 11:36:40 +0000

    [SASS-10518]Added model unit tests for api 1867

---
+++ test/models/connector/api_1867/IncomeSourceTypeSpec.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.connector.api_1867
+
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json._
+
+class IncomeSourceTypeSpec extends PlaySpec {
+
+  "IncomeSourceType" should {
+
+    "write UkProperty to JSON" in {
+      val ukProperty: IncomeSourceType = UkProperty
+      val json = Json.toJson(ukProperty)
+      json mustBe JsString("02")
+    }
+
+    "write ForeignProperty to JSON" in {
+      val foreignProperty: IncomeSourceType = ForeignProperty
+      val json = Json.toJson(foreignProperty)
+      json mustBe JsString("15")
+    }
+
+    "read JSON to UkProperty" in {
+      val json = JsString("02")
+      val result = json.validate[IncomeSourceType]
+      result mustBe JsSuccess(UkProperty)
+    }
+
+    "read JSON to ForeignProperty" in {
+      val json = JsString("15")
+      val result = json.validate[IncomeSourceType]
+      result mustBe JsSuccess(ForeignProperty)
+    }
+
+    "fail to read invalid JSON" in {
+      val json = JsString("invalid")
+      val result = json.validate[IncomeSourceType]
+      result mustBe a[JsError]
+    }
+  }
+}
